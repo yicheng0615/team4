0d1a17cf669ad67fdc3e341a2d37429627c951b2
陳崚榛 <s106213055@mail1.ncnu.edu.tw>
2020-11-19 18:53:11 +0800

    update title

--- MVC/teacherForm.php
+++ MVC/teacherForm.php
@@ -6,7 +6,7 @@ require("dbconnect.php");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
+<title>Teacher Form</title>
 </head>
 <body>
 <h1>Teacher Page</h1>
