6110aaaf5df58cda9eec66a1b08999b4f3236df9
judy_hsieh <s107213023@mial1.ncnu.edu.tw>
2020-11-22 17:53:29 +0800

    add user role in login page

--- MVC/loginCheck.php
+++ MVC/loginCheck.php
@@ -7,7 +7,19 @@ $passWord = $_POST['student_pwd'];
 
 if (checkUserIDPwd($userName, $passWord)) {
     $_SESSION['uID']=checkUserIDPwd($userName, $passWord); //設為student name
-	header("Location: student_table.php"); //登入直接前往已完成的工作頁面
+    //依權限導致不同畫面
+    if($_SESSION['uID'] == "student"){
+        header("Location: student_table.php"); 
+    }
+    elseif($_SESSION['uID'] == "teacher"){
+        header("Location: teacherTable.php");
+    }
+    elseif($_SESSION['uID'] == "secretary"){
+        header("Location: secretaryTable.php");
+    }
+    elseif($_SESSION['uID'] == "principle"){
+        header("Location: principleTable.php");
+    }
 } else { //帳密錯誤
     $_SESSION['uID']="";
     $error_msg= "帳號或密碼錯誤 請再試一次 <br />";
